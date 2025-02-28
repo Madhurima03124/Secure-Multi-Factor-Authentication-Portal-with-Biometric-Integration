e6d385b52c01e3525bfc5b0ccfb06b7085474978
Madhurima Gangireddy <116661081+Madhurima03124@users.noreply.github.com>
2025-02-28 21:36:21 +0530

    Update email_otp.py

--- Final_Projects/OTP/Email-OTP-Verification-with-Flask/email_otp.py
+++ Final_Projects/OTP/Email-OTP-Verification-with-Flask/email_otp.py
@@ -7,7 +7,7 @@ def generateOTP(otp_size = 6):
         final_otp = final_otp + str(random.randint(0,9))
     return final_otp
 
-def sendEmailVerificationRequest(sender="vyshnavigrandhi14@gmail.com",receiver="vyshnavigrandhi14gmail.com", custom_text="Hello, Your OTP is "):
+def sendEmailVerificationRequest(sender="gangireddymadhurima@gmail.com",receiver="madhurimagangireddy@gmail.com", custom_text="Hello, Your OTP is "):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
     google_app_password = "bgow bzmz erwf xyvf"
